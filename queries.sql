3b94397d2206329f7bb5714a5269d2c16a851ef3
Jose L. Guzman <43694760+jguzm047@users.noreply.github.com>
2019-12-05 14:18:19 -0800

    Update queries.sql
    Added queries for 6 and 7

--- queries.sql
+++ queries.sql
@@ -15,3 +15,12 @@ INSERT INTO Booking
 VALUES (bID, (SELECT customerID FROM Customer WHERE fName=firstName AND lName=lastName), hotelID, roomNo, bookingDate, noOfPeople, price);
 
 /* Jose doing 6 + 7 */
+INSERT INTO Assigned
+VALUES(StaffID, HotelID, roomNo);
+
+/* 7 */ 
+INSERT INTO Repair
+VALUES (hotelID, roomNo, repairDate);
+
+INSERT INTO Request
+VALUES (ManagerID, repairID, repairDate);
