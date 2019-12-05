052a5d0bd3457f5c253dd5c6470028a78a0ebdb2
Eric <eric.chaing.official@gmail.com>
2019-12-04 16:51:31 -0800

    initial commit. has all of the insert queries

---
+++ queries.sql
@@ -0,0 +1,11 @@
+INSERT INTO Customer 
+VALUES (customerID, fName, lName, Address, phNo, DOB, gender);
+
+INSERT INTO Room 
+VALUES (hotelID, roomNo, roomType);
+
+INSERT INTO MaintenanceCompany
+VALUES (cmpID, name, address , isCertified);
+
+INSERT INTO Repair 
+VALUES (rID, hotelID, roomNo, mCompany, repairDate, description, repairType
