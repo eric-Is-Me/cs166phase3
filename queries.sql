1bc70bc9bdc5104ec4f70caa92dde9adec0f8fd5
Eric <eric.chaing.official@gmail.com>
2019-12-05 13:57:38 -0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-INSERT INTO Customer 
+INSERT INTO Customer
 VALUES (customerID, fName, lName, Address, phNo, DOB, gender);
 
 INSERT INTO Room 
@@ -10,4 +10,8 @@ VALUES (cmpID, name, address , isCertified);
 INSERT INTO Repair 
 VALUES (rID, hotelID, roomNo, mCompany, repairDate, description, repairType);
 
+/* number 5 */
+INSERT INTO Booking
+VALUES (bID, (SELECT customerID FROM Customer WHERE fName=firstName AND lName=lastName), hotelID, roomNo, bookingDate, noOfPeople, price);
 
+/* Jose doing 6 + 7 */
