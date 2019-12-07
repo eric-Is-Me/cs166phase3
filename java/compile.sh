7bf408f11f58c4d734cc36624456ad30c350fddb
Jose L. Guzman <43694760+jguzm047@users.noreply.github.com>
2019-12-06 16:00:53 -0800

    Update compile.sh
    index sql script

--- java/compile.sh
+++ java/compile.sh
@@ -6,6 +6,9 @@ export PATH=$JAVA_HOME/bin:$PATH
 # Export classpath with the postgressql driver
 export CLASSPATH=$CLASSPATH:$PWD/pg73jdbc3.jar
 
+#Index tuning (NOTE:Might be wrong) 
+psql −h localhost −p $PGPORT $USER” DB” < ../sql/create_index.sql
+
 # compile the java program
 javac DBProject.java
 
